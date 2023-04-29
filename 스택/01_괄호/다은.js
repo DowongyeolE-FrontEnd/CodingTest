fe00c5b4f3a3a1c931c1477a0c35ab33da5e80e1
daeun heo <60952506+nno3onn@users.noreply.github.com>
2023-04-29 22:32:32 +0900

    Update 다은.js

--- 스택/01_괄호/다은.js
+++ 스택/01_괄호/다은.js
@@ -1,1 +1,19 @@
+function solution(n, strings) {
+  for (let i = 0; i < n; i++) {
+    const string = strings[i].split("");
+    const stack = [];
+    let result = "YES";
 
+    for (let j = 0; j < string.length; j++) {
+      if (string[j] === "(") stack.push(string[j]);
+      else {
+        if (!stack.pop()) {
+          result = "NO";
+          break;
+        }
+      }
+    }
+    if (stack.length) result = "NO";
+    console.log(result);
+  }
+}
